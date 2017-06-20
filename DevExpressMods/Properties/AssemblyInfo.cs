12ede352dff904bdbd6e2ba51d515b8830114510
jnm2 <me@jnm2.com>
2017-06-19 23:26:21 -0400

    Version 1.1.0

--- DevExpressMods/Properties/AssemblyInfo.cs
+++ DevExpressMods/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@
 [assembly: AssemblyProduct("DevExpressMods")]
 [assembly: AssemblyCopyright("MIT License")]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
